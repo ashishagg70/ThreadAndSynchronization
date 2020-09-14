8f68a766f70729c88d282c59c47dfa9670f2da32
Ashish Aggarwal <ashishaggarwal@Ashishs-MacBook-Air.local>
2020-09-14 16:39:28 +0530

    hare solution

--- 3_race/hare_tortoise.c
+++ 3_race/hare_tortoise.c
@@ -36,14 +36,63 @@ void* Hare(void *race);
 void* Randomizer(void *race);
 void* Report(void *race);
 
+//my variables
+int harePositon=0;
+int turtlePosition=0;
+int winner=-1;
+struct condition cvp; //randomizer
+struct condition cvh; //hare
+struct condition cvt; //turtle
+struct condition cvr; // report
+int pdone=0;
+int hdone=0;
+int tdone=0;
+int rdone=1;
+struct lock mutex1;
+//struct lock mutex2;
+int globalClock=0;
+
 char init(struct race *race)
 {
-	
-	return '-'; 
+	pthread_t *threadHandles = malloc(4*sizeof(*threadHandles));
+	lock_init(&mutex1);
+	//lock_init(&mutex2);
+	cond_init(&cvr);
+	cond_init(&cvp);
+	cond_init(&cvt);
+	cond_init(&cvh);
+	pthread_create(&threadHandles[0],NULL, Turtle, race);
+	pthread_create(&threadHandles[1],NULL, Hare, race);
+	pthread_create(&threadHandles[2],NULL, Randomizer, race);
+	pthread_create(&threadHandles[3],NULL, Report, race);
+
+	for(long long i=0; i < 4; i++){
+			pthread_join(threadHandles[i], NULL);
+		}
+	return winner;
 }
 
 void* Turtle(void *arg)
 {
+	struct race *race=((struct race*)arg);
+	while(winner==-1){
+		lock_acquire(&mutex1);
+		while(hdone!=1)
+			cond_wait(&cvt, &mutex1);
+		hdone=0;
+		if(winner==-1 || winner=='h'){
+			turtlePosition+=race->tortoise_speed;
+			if(turtlePosition>=race->finish_distance)
+				winner='T';
+			else if(winner=='h'){
+				winner='H';
+			}
+		}
+
+		tdone=1;
+		cond_signal(&cvr, &mutex1);
+		lock_release(&mutex1);
+	}
 	
 	return NULL;
   
@@ -51,6 +100,47 @@ void* Turtle(void *arg)
 
 void* Hare(void *arg)
 {
+	struct race *race=((struct race*)arg);
+	int isSleeping=0;
+	int localclock=0;
+	while(winner==-1){
+		lock_acquire(&mutex1);
+		while(pdone!=1)
+			cond_wait(&cvh, &mutex1);
+		pdone=0;
+		if(winner==-1)
+		{
+			if(localclock==race->hare_sleep_time && isSleeping)
+			{
+				localclock=0;
+				isSleeping=0;
+			}
+			else if(isSleeping){
+				localclock++;
+			}
+			else if(!isSleeping){
+				if((harePositon-turtlePosition)>race->hare_turtle_distance_for_sleep)
+				{
+					if(race->hare_sleep_time!=0)
+						isSleeping=1;
+				}
+					
+				else{
+					harePositon+=race->hare_speed;
+					if(harePositon>=race->finish_distance)
+						winner='h';
+					if(race->hare_sleep_time!=0)
+						isSleeping=1;
+				}
+
+			}
+		}
+		
+		hdone=1;
+		cond_signal(&cvt, &mutex1);
+		lock_release(&mutex1);
+
+	}
 	
 	return NULL;
 }
@@ -58,12 +148,69 @@ void* Hare(void *arg)
 
 void* Randomizer(void *arg)
 {
-	
+
+	int i=0;
+	struct race *race=((struct race*)arg);
+	while(winner==-1){
+		
+		lock_acquire(&mutex1);
+		while(rdone!=1)
+			cond_wait(&cvp, &mutex1);
+		rdone=0;
+		if(winner==-1){
+			if(i<race->repositioning_count && race->reposition[i].time==globalClock){
+				//printf("i: %d| race->repsiton->time: %d, globalTime: %d\n", i, race->reposition[i].time, globalClock);
+				if(race->reposition[i].player=='T'){
+					//printf("T\n");
+					turtlePosition+=race->reposition[i].distance;
+					if(turtlePosition>=race->finish_distance){
+						winner='T';
+					}
+				}else{
+					//printf("H\n");
+					//printf("HarePositino: %d| change: %d\n", harePositon, race->reposition[i].distance);
+					harePositon+=race->reposition[i].distance;
+					if(harePositon>=race->finish_distance){
+						winner='H';
+					}
+				}
+				i++;
+			}
+		}
+
+		pdone=1;
+		cond_signal(&cvh, &mutex1);
+		lock_release(&mutex1);
+
+	}
 	return NULL;
 }
 
 void* Report(void *arg)
 {
+
+	struct race *race=((struct race*)arg);
+	int localclock=0;
+	while(winner==-1){
+		lock_acquire(&mutex1);
+		while(tdone!=1)
+			cond_wait(&cvr, &mutex1);
+		tdone=0;
+		localclock++;
+		if(winner==-1){
+			if(race->printing_delay==localclock){
+				localclock=0;
+				printf("Global Clock: %d unit done| Position of Hare: %d\n",globalClock+1,  harePositon);
+				printf("Global Clock: %d unit done| Position of tortoise: %d\n", globalClock+1, turtlePosition);
+
+			}
+
+		}
+		globalClock++;
+		rdone=1;
+		cond_signal(&cvp, &mutex1);
+		lock_release(&mutex1);
+	}
 	
 	return NULL;
 }
